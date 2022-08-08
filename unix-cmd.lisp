d928b260d82a9dfc29956339a1679ea9e7ee112c
Higashi, Hiromitsu <supercaveman@gmail.com>
2022-08-08 16:10:06 +0900

    2

--- unix-cmd.lisp
+++ unix-cmd.lisp
@@ -14,3 +14,23 @@
   (uiop:directory-files path))		; => LS
 ;; (ls "~/test/")				; => (#P"/home/hiro/test/fuga" #P"/home/hiro/test/hoge")
 
+(defun cat (&rest files)
+  (dolist (f (apply #'directory files))
+    (with-open-file (in f :direction :input)
+      (loop :for line = (read-line in nil nil)
+	    :while line
+	    :do (format t "~a~%" line))))) ; => CAT
+
+;;(cat "~/*.txt")		; =>
+
+(defun touch (file)
+    (close (open "~/f.txt" :direction :probe
+			   :if-does-not-exist :create 
+			   :if-exists :append))) ; => TOUCH
+;;(touch "hoge.txt")				 ; => T
+
+(defun rm (file)
+  (delete-file file))			; => RM
+
+;;(rm "~/test/fuga")			; => T
+
