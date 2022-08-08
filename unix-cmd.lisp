4402dee7bdf00614e421f0aa87d5c7eaa21d1928
Higashi, Hiromitsu <supercaveman@gmail.com>
2022-08-08 22:16:58 +0900

    add date
    but i not sure about time-zone.

--- unix-cmd.lisp
+++ unix-cmd.lisp
@@ -62,4 +62,9 @@
 (defun popd ()
   (cd (pop directory-stack)))		; => POPD
 
+(defun date ()
+  (multiple-value-bind (sec min hr date mon yr dow daylight-p zone)
+      (decode-universal-time (get-universal-time))
+    (let ((jdow (nth dow '("月" "火" "水" "木" "金" "土" "日"))))
+      (format t "~d年 ~d月 ~d日 ~a曜日 ~2,'0d:~2,'0d:~2,'0d " yr mon date jdow hr min sec)))) ; => DATE
 
