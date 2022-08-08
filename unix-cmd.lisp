5e1cdbb4241f2ae459195806b6bff34ff2a18886
Higashi, Hiromitsu <supercaveman@gmail.com>
2022-08-08 15:07:00 +0900

    1

---
+++ unix-cmd.lisp
@@ -0,0 +1,16 @@
+﻿(defun pwd ()
+  (uiop:getcwd))			; => PWD
+
+;; (pwd)					; => #P"/home/hiro/howm/junk/"
+
+(defun cd (path)
+  (uiop:chdir path))			; => CD
+
+;; (cd "~/howm")				; => 0
+;; (pwd)					; => #P"/home/hiro/howm/"
+
+
+(defun ls (path)
+  (uiop:directory-files path))		; => LS
+;; (ls "~/test/")				; => (#P"/home/hiro/test/fuga" #P"/home/hiro/test/hoge")
+
